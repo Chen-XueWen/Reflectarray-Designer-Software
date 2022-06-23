5ac182ead9a97e33553f90869bece3496ae4b169
Tan Xue Wen <33597628+Chen-XueWen@users.noreply.github.com>
2022-06-23 14:43:38 +0800

    Delete MATLABCode directory

--- MATLABCode/Comparison_GainvsFreq.m
+++
@@ -1,9 +0,0 @@
-[file,path] = uigetfile('*.csv');
-Directory = strcat(path,file);
-Data = csvread(Directory);
-plot(Data(:,1),Data(:,2),'-k');
-hold on;
-plot(Data(:,1),Data(:,3),'--k');
-ylabel('Maximum Gain / dB');
-xlabel('Frequency / GHz');
-legend('Measured','Simulated');
